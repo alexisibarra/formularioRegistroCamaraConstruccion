3c6d88faa2d51369431e9739fd12b0fd186dd3c8
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2014-06-04 14:08:21 -0430

    Archivo con datos para la conexión y trabajo de SwiftMailer

---
+++ Controller/swiftMailerValues.php
@@ -0,0 +1,13 @@
+<?
+    $transportUsername = "direccion@ejemplo.com";
+    $transportPassword = "password";
+		$from = array(
+			"direccion@ejemplo.com" => "Nombre Ejemplo",
+		);
+		$to = array(
+			"direccion@ejemplo.com" => "Nombre Ejemplo",
+		);
+		$replyTo = array(
+			"direccion@ejemplo.com" => "Nombre Ejemplo",
+		);
+?>
